406c9838c8ae6e1b16ee789663f39770f772fb68
authdev2 <flaviogomes05.work@gmail.com>
2025-09-07 02:03:56 +0100

    Adicionado varias rotas para o site de advocacia

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,10 +3,22 @@ import bcrypt from 'bcryptjs';
 
 let password_hash = "";
 
+
+export const loginUsers = (req, res) => {
+    con.query("SELECT * FROM users", function (err, result) {
+        if (err) {
+            return res.status(500).send({ error: "Erro interno do servidor" });
+        }
+        if (result.length > 0) {
+            res.send({ message: "Usuários encontrados", result: result, success: true });
+        } else {
+            res.send({ message: "Usuários não encontrados", success: false });
+        }
+    });
+};
+
 export const postLogin = (req, res) => {
-    const email = req.params.email;
-    const password = req.params.password;
-    
+    let { email, password } = req.body;
     con.query("SELECT * FROM users WHERE email = ?", [email], function (err, result) {
         console.log("Result:", result);
         if (err) {
@@ -27,7 +39,7 @@ export const postLogin = (req, res) => {
             }
             
             console.log("Login inserido:", email, password_hash);
-            res.send({ message: "Usuário criado com sucesso", result: result });
+            res.send({ message: "Usuário criado com sucesso", result: result, success: true });
         });
     });
 };
@@ -51,5 +63,27 @@ export const loginExists = (req, res) => {
         } else {
             res.status(401).send({ error: "Email ou senha incorretos" });
         }
+    });
+};
+
+export const deleteUser = (req, res) => {
+    const { email } = req.body;
+    
+    con.query("DELETE FROM users WHERE email = ?", [email], function (err, result) {
+        if (err) {
+            return res.status(500).send({ error: "Erro ao deletar usuário" });
+        }
+        res.send({ message: "Usuário deletado com sucesso", success: true });
+    });
+};
+
+export const updateUser = (req, res) => {
+    const { email, password } = req.body;
+    
+    con.query("UPDATE users SET password = ? WHERE email = ?", [password, email], function (err, result) {
+        if (err) {
+            return res.status(500).send({ error: "Erro ao atualizar usuário" });
+        }
+        res.send({ message: "Usuário atualizado com sucesso" });
     });
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { getNoticias, postNoticias } from './routes/noticias.js';
-import {postLogin, loginExists } from './routes/login.js';
+import {postLogin, loginExists, deleteUser, loginUsers } from './routes/login.js';
 
 let app = express();
 app.use(cors());
@@ -14,8 +14,11 @@ app.post("/noticias/:title/:description/:image", postNoticias);
 
 
 //LOGUN
-app.post("/login/:email/:password", postLogin);
+app.get("/login/users", loginUsers);
+app.post("/login/create", postLogin);
 app.post("/login", loginExists);
+
+app.delete("/login/delete", deleteUser);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
